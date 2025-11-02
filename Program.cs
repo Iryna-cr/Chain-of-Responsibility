94f66218b61ebb7a7e438989a6742e7401c73c7c
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 23:41:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class OrderHandler
+{
+    private OrderHandler next;
+
+    public void SetNext(OrderHandler n) => next = n;
+
+    public virtual void Handle(string request)
+    {
+        if (next != null) next.Handle(request);
+    }
+}
+
+class PaymentHandler : OrderHandler
+{
+    public override void Handle(string request)
+    {
+        if (request == "оплата") Console.WriteLine("Оплата оброблена");
+        else base.Handle(request);
+    }
+}
+
+class DeliveryHandler : OrderHandler
+{
+    public override void Handle(string request)
+    {
+        if (request == "доставка") Console.WriteLine("Доставка оформлена");
+        else base.Handle(request);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var payment = new PaymentHandler();
+        var delivery = new DeliveryHandler();
+        payment.SetNext(delivery);
+
+        payment.Handle("доставка");
+    }
+}
+
+
+
+
+
+
+
+
+
